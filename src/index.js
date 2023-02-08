13926ca7f3831cbdb8cf32945247eeaab287eae1
Armando Cruz Cruz <armando.cruz.x.cruz@gmail.com>
2023-02-08 17:22:08 -0600

    eliminacion de archivos
    Eliminacion de archivos no necesarios

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const Hello = (props) => 
+  (<div><p>Hello {props.name}, you are {props.age} years old</p></div>)
+
+const App = () => {
+  const name = 'Peter'
+  const age = 18
+  return (
+    <div>
+        <h1>Greetings</h1>
+        <Hello name={name} age={age} />
+        <Hello name='Mary' age={18+3} />
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -10,8 +22,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
